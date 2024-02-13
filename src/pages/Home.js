7d7ed597619bb9ab6e08be6aa9b6e80e0d28a931
Collins87mbathi <perezcollins87@gmail.com>
2024-02-13 23:12:36 +0300

    Fix: fixed page location issue

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../components/heroSection/Hero';
+import Main from '../components/mainpage/Main';
+import Testimonial from '../components/testimonials/Testimonial';
+
+function Home() {
+    return (
+        <div>
+            <Hero />
+            <Main />
+            <Testimonial />
+
+
+        </div>
+    )
+}
+
+export default Home;
+
+
+
+
